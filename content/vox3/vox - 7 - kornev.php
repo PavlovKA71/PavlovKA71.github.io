fa693f463914e7fa41affe872d63ed153ad8ea4a
vox <robot@vox-journal.org>
2023-03-13 23:56:16 +0300

    2023-03-13:23:56:11

---
+++ content/vox3/vox - 7 - kornev.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<title>VOX - философский журнал</title>
+	<link rel="STYLESHEET" type="text/css" href="../index.css" />
+</head>
+<body>
+	<table id="header2">
+		<tr>
+			<td rowspan="2" id="logo2"><a href="../index.php"><img src="../images/logo2.gif" width="199" height="99" alt="VOX - философский журнал" /></a></td>
+			<td id="search2">
+			<form action="../search.php">
+			Поиск: <input name="search" class="field"/>
+			<a href="javascript:document.forms.item(0).submit();"><img src="../images/search.gif" alt="Поиск" /></a>
+			</form>
+			</td>
+		</tr>
+		<tr>
+			<td id="menu">
+				<a href="../journal.php">О журнале</a>
+				<a href="../authors.php">Редакция</a>
+				<a href="../contacts.php">Контакты</a>
+			</td>
+		</tr>
+	</table>
+<table id="path">
+	<tr>
+		<td class="volume"><a href="index.php">Выпуск №7 декабрь, 2009</a></td>
+		<td class="section">ПЕРВОЕ СЛОВО</td>
+	</tr>
+</table>
+
+<div class="text">
+
+
+<div class=Section1>
+
+
+<h1>Философия у-держания бытия</h1>
+<h2>С. Корнев</h2>
+
+<p class=MsoBodyText>Наделить человека определенной мерой, да и вообще всяким достоинством способны лишь те вещи, которые своим ярким светом ослепляют глаза смотрящего. Спустя непродолжительное время, человек, прозрев от ослепления, вновь может взирать на источник этого яркого света. Вещь является человеку уже иной, как бы в другом свете. Идеи космоса, единства, истины и т. д. - вещи, манящие своей яркостью. Устремление к свету этих вещей наделяет наше внимание к ним особой отметкой. Эта отметка не только наделение человека достоинством исторического масштаба, но и лавровый венок от самого времени. И время, будучи судьбой этой самой истории, наделит человека той мерой достоинства, с которой он был внимателен к свету этих вещей.</p>
+
+<p><a href="vox - 7 - kornev.doc">Полный текст</a></p>
+
+</div>
+
+</div>
+
+	</div>
+
+<div class="text">
+<a link="comments"></a>
+<div class="comments">
+	<div class="title">Отзывы</div>
+	<form action="#" method="post">
+	Имя<br />
+	<input name="comment_sender" /><br />
+	Текст<br />
+	<textarea name="comment_body" rows="5" cols="40"></textarea>
+	<div class="add_comment">
+		<a href="javascript:document.forms.item(1).submit();">Добавить отзыв</a>
+	</div>
+	</form>
+</div>
+</div>	<div class="footer">
+		<div>Издательство ИФРАН, Москва 2006</div>
+
+		<div>© Коллектив авторов</div>
+		<div>© ИФРАН</div>
+	</div>
+</body>
+</html>
---
+++ content/vox3/vox - 7 - krishtaleva.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<title>VOX - философский журнал</title>
+	<link rel="STYLESHEET" type="text/css" href="../index.css" />
+</head>
+<body>
+	<table id="header2">
+		<tr>
+			<td rowspan="2" id="logo2"><a href="../index.php"><img src="../images/logo2.gif" width="199" height="99" alt="VOX - философский журнал" /></a></td>
+			<td id="search2">
+			<form action="../search.php">
+			Поиск: <input name="search" class="field"/>
+			<a href="javascript:document.forms.item(0).submit();"><img src="../images/search.gif" alt="Поиск" /></a>
+			</form>
+			</td>
+		</tr>
+		<tr>
+			<td id="menu">
+				<a href="../journal.php">О журнале</a>
+				<a href="../authors.php">Редакция</a>
+				<a href="../contacts.php">Контакты</a>
+			</td>
+		</tr>
+	</table>
+<table id="path">
+	<tr>
+		<td class="volume"><a href="index.php">Выпуск №7 декабрь, 2009</a></td>
+		<td class="section">ПЕРВОЕ СЛОВО</td>
+	</tr>
+</table>
+
+<div class="text">
+
+
+<div class=Section1>
+
+
+<h1>Поздний стоицизм Сенеки: поступок мысли</h1>
+<h2>Л. Кришталева</h2>
+
+<p>Эллинизм – эпоха непрерывных войн и пертурбаций. Исследователи считают, что политическая и социальная нестабильность привели к тому, что люди, занимавшиеся философией, были увлечены в большей степени проблемами счастья, добродетели, нежели отвлеченными вопросами о бытии и устройстве мира. Не случайно, и в стоицизме, и в эпикурействе, которые наряду со скептицизмом являлись самими влиятельными философскими школами этого периода, высшим блаженством, несмотря на различие учений, являются аналогичные состояния: бесчувствие (апатия) и безмятежность (атараксия). Считалось, что постоянство счастья достигается благодаря самодостаточности человека, владению своей душой, освобождению от случайностей внешнего мира. Эти сравнения ставят вопрос о своеобразии стоицизма.</p>
+
+<p><a href="vox - 7 - krishtaleva.doc">Полный текст</a></p>
+
+</div>
+
+</div>
+
+	</div>
+
+<div class="text">
+<a link="comments"></a>
+<div class="comments">
+	<div class="title">Отзывы</div>
+	<form action="#" method="post">
+	Имя<br />
+	<input name="comment_sender" /><br />
+	Текст<br />
+	<textarea name="comment_body" rows="5" cols="40"></textarea>
+	<div class="add_comment">
+		<a href="javascript:document.forms.item(1).submit();">Добавить отзыв</a>
+	</div>
+	</form>
+</div>
+</div>	<div class="footer">
+		<div>Издательство ИФРАН, Москва 2006</div>
+
+		<div>© Коллектив авторов</div>
+		<div>© ИФРАН</div>
+	</div>
+</body>
+</html>
